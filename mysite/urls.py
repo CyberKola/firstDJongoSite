a581ac52571501046e63ba081ff8ee47ae222d27
abdulla <abdullaerbolatuli@gmail.com>
2026-03-17 16:42:00 +0500

    Лишь создал приложение новости

--- mysite/urls.py
+++ mysite/urls.py
@@ -24,5 +24,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    path('news/', include('news.urls')),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
