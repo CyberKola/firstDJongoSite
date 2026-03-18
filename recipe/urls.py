72f681cf833a65803a68761044d59a6374f76477
Abdulla <s@ksa.edu>
2026-03-19 00:53:50 +0500

    добавил переходную кнопку на приложение рецепты в главном экране

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,7 +5,7 @@ from recipe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.recipes),
+    path('', views.recipes, name='recipes'),
     path('update_recipe/<id>', views.update_recipe, name='update_recipe'),
     path('delete_recipe/<id>', views.delete_recipe, name='delete_recipe'),
 ]
